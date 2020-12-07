0929c2c6c9b2283826b87b8efa912a702f6f141c
Samuel Johnson <samrm310@gmail.com>
2020-12-07 02:49:51 -0300

    Melhorado interação com o slide

--- javaScript/scriptpagvampiros.js
+++ javaScript/scriptpagvampiros.js
@@ -1,14 +1,28 @@
-let label_carrocel = document.getElementsByName('label-fotos')
+let label_carrossel = document.getElementsByClassName('label-fotos')
+
+let img_carrocel = document.getElementsByName('img-carrossel')
 
 
-label_carrocel[0].addEventListener('click', function() {
-    label_carrocel[0].style.backgroundColor = "pink";
-    label_carrocel[1].style.backgroundColor = "white";
+
+label_carrossel[0].addEventListener('click', function () {
+    limparCor_label()
+    label_carrossel[0].style.backgroundColor = "purple";
 });
 
 
-label_carrocel[1].addEventListener('click',function() {
-    label_carrocel[1].style.backgroundColor = "pink";
-    label_carrocel[0].style.backgroundColor = "white";
+label_carrossel[1].addEventListener('click', function () {
+    limparCor_label()
+    label_carrossel[1].style.backgroundColor = "purple";
 });
 
+label_carrossel[2].addEventListener('click', function () {
+    limparCor_label()
+    label_carrossel[2].style.backgroundColor = "purple";
+});
+
+
+function limparCor_label(){
+    for (var cont = 0;cont<=label_carrossel.length-1;cont++) {
+        label_carrossel[cont].style.backgroundColor = "white"; 
+    }  
+}
