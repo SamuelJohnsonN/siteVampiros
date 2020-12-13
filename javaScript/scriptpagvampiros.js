713432efdf23c48f79a33c45fcd0503f80c49266
Samuel Johnson <samrm310@gmail.com>
2020-12-13 01:26:39 -0300

    Finalizando site

--- javaScript/scriptpagvampiros.js
+++ javaScript/scriptpagvampiros.js
@@ -3,24 +3,24 @@ let label_carrossel = document.getElementsByClassName('label-fotos')
 let img_carrocel = document.getElementsByName('img-carrossel')
 
 window.onload = function() {
-    label_carrossel[0].style.backgroundColor = "purple";
+    label_carrossel[0].style.backgroundColor = "#7b5fe2";
 }
 
 
 label_carrossel[0].addEventListener('click', function () {
     limparCor_label()
-    label_carrossel[0].style.backgroundColor = "purple";
+    label_carrossel[0].style.backgroundColor = "#7b5fe2";
 });
 
 
 label_carrossel[1].addEventListener('click', function () {
     limparCor_label()
-    label_carrossel[1].style.backgroundColor = "purple";
+    label_carrossel[1].style.backgroundColor = "#7b5fe2";
 });
 
 label_carrossel[2].addEventListener('click', function () {
     limparCor_label()
-    label_carrossel[2].style.backgroundColor = "purple";
+    label_carrossel[2].style.backgroundColor = "#7b5fe2";
 });
 
 
