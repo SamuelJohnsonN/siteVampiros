abfea23b96b8a097e43a1fe79822ee3329513f42
Samuel Johnson <samrm310@gmail.com>
2020-12-13 03:46:46 -0300

    melhorado informações do media

--- javaScript/scriptpagvampiros.js
+++ javaScript/scriptpagvampiros.js
@@ -1,10 +1,10 @@
-let label_carrossel = document.getElementsByClassName('label-fotos')
+let label_carrossel = document.getElementsByClassName('label-fotos');
+let img_carrocel = document.getElementsByName('img-carrossel');
 
-let img_carrocel = document.getElementsByName('img-carrossel')
 
 window.onload = function() {
     label_carrossel[0].style.backgroundColor = "#7b5fe2";
-}
+};
 
 
 label_carrossel[0].addEventListener('click', function () {
@@ -23,9 +23,25 @@ label_carrossel[2].addEventListener('click', function () {
     label_carrossel[2].style.backgroundColor = "#7b5fe2";
 });
 
-
 function limparCor_label(){
     for (var cont = 0;cont<=label_carrossel.length-1;cont++) {
         label_carrossel[cont].style.backgroundColor = "white"; 
-    }  
-}
+    }; 
+};
+
+let img_vamp = document.getElementById("img-vamp");
+let img_cacador = document.getElementById("img-caçador");
+let img_child = document.getElementById("img-child");
+
+window.onresize = function (event) {
+    if(window.innerWidth > 1355) {
+        img_vamp.src = "imagens/vampiros-1920.jpg";
+        img_cacador.src = "imagens/caçador-1920.jpg";
+        img_child.src = "imagens/vampiro-child-1920.jpg";
+    }else{
+        img_vamp.src = "imagens/vampiros.jpg";
+        img_cacador.src = "imagens/caçador.jpg";
+        img_child.src = "imagens/vampiro-child.jpg";
+    };
+
+};
